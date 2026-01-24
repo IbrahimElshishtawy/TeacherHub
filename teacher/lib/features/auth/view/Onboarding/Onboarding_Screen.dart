7637e1dfdb9902dce53784b777ebfd85a1b89271
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-24 20:25:01 +0200

    Update Onboarding_Screen.dart

--- teacher/lib/features/auth/view/Onboarding/Onboarding_Screen.dart
+++ teacher/lib/features/auth/view/Onboarding/Onboarding_Screen.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: file_names, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:teacher/features/auth/view/user_type/User_Type_Screen.dart';
+import 'widget/onboarding_page.dart';
+import 'widget/dots_indicator.dart';
+import 'widget/next_button.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -13,82 +14,52 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final PageController _pageController = PageController();
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
+      body: Column(
         children: [
-          _buildOnboardingPage(
-            lottieAsset: 'assets/lottie/Welcome.json',
-            text: 'مرحباً! اكتشف طريقة جديدة لتعلم الدروس بسهولة وبشكل منظم.',
+          Align(
+            alignment: Alignment.topCenter, // جعل البداية من أعلى
+            child: Container(
+              padding: EdgeInsets.only(top: 50), // إضافة المسافة من الأعلى
+              child: PageView(
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                children: [
+                  OnboardingPage(
+                    lottieAsset: 'assets/lottie/Welcome.json',
+                    text:
+                        'مرحباً! اكتشف طريقة جديدة لتعلم الدروس بسهولة وبشكل منظم.',
+                  ),
+                  OnboardingPage(
+                    lottieAsset: 'assets/lottie/Student transparent.json',
+                    text:
+                        'جميع الدروس والملاحظات الآن في مكان واحد لسهولة الوصول.',
+                  ),
+                  OnboardingPage(
+                    lottieAsset: 'assets/lottie/sign in.json',
+                    text:
+                        'ابدأ رحلتك التعليمية الآن واستمتع بتجربة تعليمية مميزة.',
+                  ),
+                ],
+              ),
+            ),
           ),
-          _buildOnboardingPage(
-            lottieAsset: 'assets/lottie/Student transparent.json',
-            text: 'جميع الدروس والملاحظات الآن في مكان واحد لسهولة الوصول.',
-          ),
-          _buildOnboardingPage(
-            lottieAsset: 'assets/lottie/sign in.json',
-            text: 'ابدأ رحلتك التعليمية الآن واستمتع بتجربة تعليمية مميزة.',
+          DotsIndicator(currentPage: _currentPage),
+          SizedBox(height: 20),
+          NextButton(
+            currentPage: _currentPage,
+            pageController: _pageController,
           ),
         ],
       ),
-    );
-  }
-
-  Widget _buildOnboardingPage({
-    required String lottieAsset,
-    required String text,
-  }) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Lottie.asset(lottieAsset, width: 250, height: 250, fit: BoxFit.fill),
-        SizedBox(height: 20),
-        Text(
-          text,
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            if (_pageController.page == 2) {
-              Navigator.pushReplacement(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      UserTypeScreen(),
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                        const begin = Offset(1.0, 0.0);
-                        const end = Offset.zero;
-                        const curve = Curves.easeInOut;
-
-                        var tween = Tween(
-                          begin: begin,
-                          end: end,
-                        ).chain(CurveTween(curve: curve));
-                        var offsetAnimation = animation.drive(tween);
-
-                        return SlideTransition(
-                          position: offsetAnimation,
-                          child: child,
-                        );
-                      },
-                ),
-              );
-            } else {
-              _pageController.nextPage(
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeIn,
-              );
-            }
-          },
-          child: Text('التالي'),
-        ),
-      ],
     );
   }
 }
