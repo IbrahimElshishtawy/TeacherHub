2169f84a15a0e2f9eca54b2a31b26e813a1e04bf
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-25 16:19:20 +0200

    Update User_Type_Screen.dart

--- teacher/lib/features/auth/view/user_type/User_Type_Screen.dart
+++ teacher/lib/features/auth/view/user_type/User_Type_Screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'widgets/user_chosea_typelogin.dart';
 
@@ -11,7 +13,7 @@ class UserTypeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Spacer(flex: 3),
+            Spacer(flex: 2),
             Text(
               "من سيستخدم التطبيق؟",
               style: TextStyle(
@@ -21,38 +23,45 @@ class UserTypeScreen extends StatelessWidget {
               ),
               textAlign: TextAlign.center,
             ),
+            SizedBox(height: 10),
             Text(
               'يساعدنا هذا في تخصيص تجربتك داخل التطبيق',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 18, color: Colors.black54),
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
+
             // Use UserChoseaTypelogin widget for each user type
             UserChoseaTypelogin(
               title: 'Admin',
               lottieAsset: 'assets/lottie/admin.json',
-              onTap: () => Navigator.pushNamed(context, '/admin_dashboard'),
+              onTap: () => Navigator.pushNamed(context, '/admin_login'),
               subtitle: 'إدارة النظام والمستخدمين',
             ),
+            SizedBox(height: 10),
             UserChoseaTypelogin(
               title: 'Teacher',
               lottieAsset: 'assets/lottie/Teacher.json',
-              onTap: () => Navigator.pushNamed(context, '/teacher_dashboard'),
+              onTap: () => Navigator.pushNamed(context, '/teacher_login'),
               subtitle: 'إنشاء وإدارة الدروس',
             ),
+            SizedBox(height: 10),
             UserChoseaTypelogin(
               title: 'Student',
               lottieAsset: 'assets/lottie/Student transparent.json',
-              onTap: () => Navigator.pushNamed(context, '/student_dashboard'),
+              onTap: () => Navigator.pushNamed(context, '/student_login'),
               subtitle: 'التعلّم ومتابعة الدروس',
             ),
+            SizedBox(height: 10),
             UserChoseaTypelogin(
               title: 'Parent',
               lottieAsset: 'assets/lottie/present.json',
-              onTap: () => Navigator.pushNamed(context, '/parent_dashboard'),
+              onTap: () => Navigator.pushNamed(context, '/parent_login'),
               subtitle: 'تابع تعلم أبنك ومتابعة دروسه بسهولة',
             ),
-            Spacer(flex: 5),
+
+            // Spacer to push the content to the top and add a gap at the bottom
+            Spacer(flex: 3),
           ],
         ),
       ),
