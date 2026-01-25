9f600aae639a1381f06f61aefb9f16986915a2cb
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-25 16:19:14 +0200

    Create student_login_screen.dart

---
+++ teacher/lib/features/auth/view/login/student_login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StudentLoginScreen extends StatelessWidget {
+  const StudentLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("تسجيل دخول الادمن")),
+      body: Center(child: Text("صفحة تسجيل دخول الادمن")),
+    );
+  }
+}
