3cbf4e9f4d750046e2590e3dd24136ee26020a4c
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-29 13:14:41 +0200

    Create AppBar_Tap.dart

---
+++ teacher/lib/features/auth/view/login/admin_login/widget/AppBar_Tap.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:teacher/features/auth/view/login/admin_login/widget/Left_Arrow_Painter.dart'; // تأكد من المسار الصحيح
+
+class AppbarTap extends StatelessWidget implements PreferredSizeWidget {
+  const AppbarTap({super.key});
+
+  @override
+  Size get preferredSize => Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: CustomPaint(size: Size(30, 30), painter: LeftArrowPainter()),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
