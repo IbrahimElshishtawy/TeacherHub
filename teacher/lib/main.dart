b7dda208b6fa04ab1a97b7a65d97c662b2460fa1
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-30 20:28:22 +0200

    Update main.dart

--- teacher/lib/main.dart
+++ teacher/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:teacher/features/auth/view/login/admin_login/admin_login_screen.dart';
 import 'package:teacher/features/auth/view/login/admin_login/forget_password_admin.dart';
 import 'package:teacher/features/auth/view/login/parent_login/parent_login_screen.dart';
+import 'package:teacher/features/auth/view/login/student_login/forget_password_student.dart';
 import 'package:teacher/features/auth/view/login/student_login/student_login_screen.dart';
+import 'package:teacher/features/auth/view/login/teacher_login/forget_password_teacher.dart';
 import 'package:teacher/features/auth/view/login/teacher_login/teacher_login_screen.dart';
 import 'features/auth/view/Onboarding/Onboarding_Screen.dart';
 import 'features/auth/view/user_type_login/User_Type_login_Screen.dart';
@@ -26,7 +28,9 @@ class MyApp extends StatelessWidget {
         '/teacher_login': (context) => TeacherLoginScreen(),
         '/student_login': (context) => StudentLoginScreen(),
         '/parent_login': (context) => ParentLoginScreen(),
-        '/forget_password': (context) => ForgotPasswordScreen(),
+        '/forget_password_admin': (context) => ForgotPasswordadminScreen(),
+        '/forget_password_teacher': (context) => ForgetPasswordTeacher(),
+        '/forget_password_student': (context) => ForgetPasswordStudent(),
       },
       home: OnboardingScreen(),
     );
