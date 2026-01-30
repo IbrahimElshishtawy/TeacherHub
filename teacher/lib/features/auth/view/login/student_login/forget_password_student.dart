ebeb08220fdf1cd2e7f6180825f1cf2e2d86ecd9
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-30 20:27:59 +0200

    Update forget_password_student.dart

--- teacher/lib/features/auth/view/login/student_login/forget_password_student.dart
+++ teacher/lib/features/auth/view/login/student_login/forget_password_student.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ForgetPasswordStudent extends StatelessWidget {
+  const ForgetPasswordStudent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
