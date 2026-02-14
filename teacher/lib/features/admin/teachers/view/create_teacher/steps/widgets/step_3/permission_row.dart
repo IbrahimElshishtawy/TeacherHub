8f328256a7498872f2f43922077133457e39ea80
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-02-14 20:38:54 +0200

    Create permission_row.dart

---
+++ teacher/lib/features/admin/teachers/view/create_teacher/steps/widgets/step_3/permission_row.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'permissions_colors.dart';
+
+class PermissionRow extends StatelessWidget {
+  final String title;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  const PermissionRow({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: [
+          Transform.scale(
+            scale: 0.95,
+            child: Switch(
+              value: value,
+              onChanged: onChanged,
+              activeThumbColor: PermColors.primary,
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w800,
+                color: PermColors.titleColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
