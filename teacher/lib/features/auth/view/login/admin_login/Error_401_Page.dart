1c774fcddc4df34cbe460bf3e4f600bd0812ad33
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-02-22 20:31:34 +0200

    Update Error_401_Page.dart

--- teacher/lib/features/auth/view/login/admin_login/Error_401_Page.dart
+++ teacher/lib/features/auth/view/login/admin_login/Error_401_Page.dart
@@ -11,14 +11,14 @@ class Error401Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('خطأ 401')),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(6.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Lottie.asset(
               'assets/lottie/404 blue.json',
-              width: 140,
-              height: 140,
+              width: 400,
+              height: 400,
             ),
             SizedBox(height: 20),
             Text(
@@ -26,16 +26,24 @@ class Error401Page extends StatelessWidget {
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // أعد المحاولة
-                Navigator.pop(context);
-              },
-              child: Text('إعادة المحاولة'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('تواصل معنا إذا استمرت المشكلة'),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text('الرجوع', style: TextStyle(color: Colors.white)),
+                ),
+              ),
             ),
           ],
         ),
