5c42fe3555971aad9a8aab54c5e1eac31934ec09
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-02-22 20:30:53 +0200

    Update reports_binding.dart

---
+++ teacher/lib/features/admin/reports/binding/reports_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import '../controller/reports_controller.dart';
+
+class ReportsBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ReportsController>(() => ReportsController());
+  }
+}
