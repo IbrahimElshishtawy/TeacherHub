e92d2e18e0d77194151cf74d685725fa8c914321
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-03-05 22:26:14 +0200

    `Added // ignore_for_file: deprecated_member_use comment and updated import statements`

--- teacher/lib/features/Student/Student_home/widgets_home/warning/view/warning_card.dart
+++ teacher/lib/features/Student/Student_home/widgets_home/warning/view/warning_card.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:teacher/features/Student/Student_home/widgets_home/warning/widgets/warning_card_header.dart';
 import 'package:teacher/features/Student/Student_home/widgets_home/warning/widgets/warning_list.dart';
